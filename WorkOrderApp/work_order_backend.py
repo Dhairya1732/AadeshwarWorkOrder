4814237ed5e81b6a131200842b18657b3ac2bd97
Dhairya Chheda <dhairyachheda1732@gmail.com>
2024-06-29 20:39:39 +0530

    Added database and its functionns

--- WorkOrderApp/work_order_backend.py
+++ WorkOrderApp/work_order_backend.py
@@ -53,13 +53,27 @@ class WorkOrderAppBackend:
 
     def generate_work_order(self):
         orders = pd.read_csv(self.csv_path)
-        orders = orders.head(1)
-        columns_to_ignore = ['Unit Price', 'TOTAL', 'SKU ID', 'Shipping Address', 'status', 'Promised Delivery Date']
+        fabric_sheet = pd.read_excel(r"D:\AadeshwarWorkOrder\Database\Fabric_Work Order revise new.xlsx",sheet_name="Fabric")
+        #inches = pd.read_excel("D:\AadeshwarWorkOrder\Database\Daily Update sheet.xlsx",sheet_name="Sheet4")
+        orders = orders.tail(1)
+        columns_to_ignore = ['Unit Price', 'TOTAL', 'Shipping Address', 'status', 'Promised Delivery Date']
         
         for index, row in orders.iterrows():
             order_data = row.to_dict()
             for column in columns_to_ignore:
                 order_data.pop(column, None)
+
+            relevant_columns = ['SKU_ID', 'Legs', 'Legs Quantity', 'Cushion Qty', 'Cushion Fabric', 'Sofa Fabric', 'Legs Finish', 'Legs Assembly', 'Cushions', 'Cushion Size']
+            fabric_sheet = fabric_sheet[relevant_columns]
+            fabric_sheet = fabric_sheet.fillna("")
+            sku_id = order_data['SKU ID'] 
+            # Check if the 'Fabric' sheet contains the 'SKU_ID' column
+            if 'SKU_ID' in fabric_sheet.columns:
+                # Iterate over the orders
+                for index, row in orders.iterrows():
+                    order_data = row.to_dict()
+                    for column in columns_to_ignore:
+                        order_data.pop(column, None)
 
             if 'Order Confirmed Date' in order_data:
                 order_data['Order Confirmed Date'] = pd.to_datetime(order_data['Order Confirmed Date'],dayfirst=True).date()
@@ -73,6 +87,18 @@ class WorkOrderAppBackend:
             current_month = datetime.now().strftime("%B")
             order_no = f"G1/{current_month}/{self.current_order_no}"
             order_data['OrderNo'] = order_no
+
+            sku_id = order_data['SKU ID']
+            if fabric_sheet['SKU_ID'].isin([sku_id]).any():
+                additional_data = fabric_sheet[fabric_sheet['SKU_ID'] == sku_id].to_dict('records')[0]
+            else:
+                additional_data= {col: "" for col in relevant_columns if col!="SKU_ID"}
+            order_data.update(additional_data)
+
+            # if inches['Sheet4'].isin([sku_id]).any():
+            #     additional_data = inches['Sheet4'].loc[inches['Sheet4'] == sku_id].to_dict('records')[0]
+            #     order_data.update(additional_data)
+
             self.create_work_order(order_data, self.foaming_template_path, f"foaming_{index+1}.pdf")
             self.create_work_order(order_data, self.carpenter_template_path, f"carpenter_{index+1}.pdf")
             self.create_work_order(order_data, self.sales_template_path, f"sales_{index+1}.pdf")
@@ -90,9 +116,9 @@ class WorkOrderAppBackend:
         except Exception as e:
             print(f"Failed to process work order {order_data['OrderNo']}: {e}")
 
-    def process_work_order(self, data, foaming_template_path, output_path, image_width=None, image_height=None):
+    def process_work_order(self, data, template_path, output_path, image_width=None, image_height=None):
         try:
-            doc = Document(foaming_template_path)
+            doc = Document(template_path)
         except Exception as e:
             print(f"Error opening template: {e}")
             return
@@ -112,6 +138,8 @@ class WorkOrderAppBackend:
                                     paragraph.text = paragraph.text.replace(f'[{key}]', str(value))
                                     for run in paragraph.runs:
                                         set_run_font(run)
+                                    if template_path==self.carpenter_template_path and key=='Order Confirmed Date':
+                                        run.font.size = Pt(16)
 
         try:
             doc.save(output_path)
