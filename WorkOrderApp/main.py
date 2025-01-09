2e6160e6034559555678dbaf17b45ac0aadd78d9
Dhairya Chheda <dhairyachheda1732@gmail.com>
2025-01-09 19:39:50 +0530

    renamed files

--- WorkOrderApp/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from work_order_ui import WorkOrderAppUI
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = WorkOrderAppUI()
-    ex.show()
-    sys.exit(app.exec_())
